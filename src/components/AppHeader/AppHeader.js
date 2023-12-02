93d1ed46ca4d3e48ce57ffdb2fd9dab2821ed8c7
hamidreza8968 <hamidreza.seydabadi68@gmail.com>
2023-12-02 10:56:56 +0100

    WIP: AppHeader

---
+++ src/components/AppHeader/AppHeader.js
@@ -0,0 +1,10 @@
+import styles from "./AppHeader.module.css";
+
+function AppHeader() {
+    return(
+        <div className={styles.title}>
+            TODO LIST
+        </div>
+    )
+}
+export default AppHeader;
